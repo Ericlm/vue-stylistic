a3c9cc4c21af50753f5e574b687db6128584ed5c
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2024-06-15 21:41:47 +0200

    ➕ Vue eslint parser

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,17 @@
 
 import eslint from '@eslint/js'
 import stylistic from '@stylistic/eslint-plugin'
+import vueParser from 'vue-eslint-parser'
 
 export default [
   eslint.configs.recommended,
   stylistic.configs['recommended-flat'],
+
+  {
+    files: ['**/*.vue'],
+
+    languageOptions: {
+      parser: vueParser,
+    },
+  },
 ]
